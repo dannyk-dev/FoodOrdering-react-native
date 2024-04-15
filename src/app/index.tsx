3fa831c40465a51ef6dd4798e91eccb3b8bc5ff6
dannyk-dev <kruger.dkk@gmail.com>
2024-04-14 23:43:37 -0300

    Auth pages

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -23,6 +23,9 @@ const index = () => {
       <Link href={"/(admin)"} asChild>
         <Button text="Admin" />
       </Link>
+      <Link href={"/sign-in"} asChild>
+        <Button text="Sign in" />
+      </Link>
     </View>
   );
 };
