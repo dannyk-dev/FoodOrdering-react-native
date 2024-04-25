c4bda6ed93a0418f1005ac13cd2dc5537e58de7a
dannyk-dev <kruger.dkk@gmail.com>
2024-04-25 13:40:40 -0300

    Supabase local setup

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -15,9 +15,8 @@ const ExpoSecureStoreAdapter = {
   },
 };
 
-const supabaseUrl = "https://vapaogswduoocyvhxfno.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZhcGFvZ3N3ZHVvb2N5dmh4Zm5vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMyNjk4ODMsImV4cCI6MjAyODg0NTg4M30.hp09Vh94KCA7YCywh3UZyNHmyjerqqe2XfRPI2PMO6M";
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || "";
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON || "";
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
   auth: {
